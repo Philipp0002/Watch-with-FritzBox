131824897bd48f31aad0af4333c9ec6e1b323fa4
Philipp0002 <philipp@raffaelhahn.de>
2025-05-05 20:16:50 +0200

    Fixes

--- app/src/main/java/de/hahnphilipp/watchwithfritzbox/epg/LogcatEpgReader.java
+++ app/src/main/java/de/hahnphilipp/watchwithfritzbox/epg/LogcatEpgReader.java
@@ -148,11 +148,6 @@ public class LogcatEpgReader {
                 int descIndex = line.indexOf("text='") + "text='".length();
                 int descEndIndex = line.charAt(line.length() - 1) == '\'' ? line.length() - 1 : line.length();
 
-                //Caused by: java.lang.StringIndexOutOfBoundsException: String index out of range: -1
-                /*if(line.length() ) {
-                    return;
-                }*/
-
                 try {
                     String desc = line.substring(descIndex, descEndIndex);
 
